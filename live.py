7299dabf945becd751ee0161ba9dac02b157640d
Najmul Hak Ansari <55761829+Bholi@users.noreply.github.com>
2025-01-24 16:03:24 +0545

    Update live.py

--- live.py
+++ live.py
@@ -29,6 +29,8 @@ def floorsheet_scraper():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1920x1080")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+    chrome_options.add_argument("--disable-gpu")  # Disable GPU hardware acceleration
+    chrome_options.add_argument("--disable-software-rasterizer")  # Disable software rasterizer
 
     # Optional: To debug by running without headless mode
     # chrome_options.headless = False  # Uncomment this line to see the browser visually during scraping
