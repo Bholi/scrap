8b56f9da49b5bb3d64827607d04dad3d5a1294b7
Najmul Hak Ansari <55761829+Bholi@users.noreply.github.com>
2025-01-24 15:23:31 +0545

    Update 9 live.py

--- live.py
+++ live.py
@@ -24,12 +24,13 @@ def floorsheet_scraper():
 
     # Set up Chrome options
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")  # Headless mode for browser efficiency
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")  # Disable GPU for performance
     chrome_options.add_argument("--window-size=1920x1080")
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-    
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")  # Set user-agent
+
     logger.info("Starting the scraper...")
 
     try:
